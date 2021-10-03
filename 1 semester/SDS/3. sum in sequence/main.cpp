43f6f514dbce62a7be8c7b3bdf131e9a351dde9a
Kizenkov_V <Kizenkov_V@15575-T490>
2021-10-03 22:06:53 +0300

    new changes in 3 task

--- 1 semester/SDS/3. sum in sequence/main.cpp
+++ 1 semester/SDS/3. sum in sequence/main.cpp
@@ -4,7 +4,6 @@
 #include<vector>
 
 using namespace std;
-#define ITEM_SIZE 5
 
 
 vector<int> get_pair_value(int val){
@@ -13,6 +12,16 @@ vector<int> get_pair_value(int val){
         }else{
             return {0,0,val,1,0};
         }
+}
+
+void increment_val(vector<vector<int> >& tree, int v, int l, int r) {
+    if (tree[v][4] != 0) {
+        tree[v] = { tree[v][2] + tree[v][3] * tree[v][4],tree[v][3],tree[v][0] + tree[v][1] * tree[v][4],tree[v][1],0 };
+        if (r != l) {
+            tree[2 * v][4] += 1;
+            tree[v * 2 + 1][4] += 1;
+        }
+    }
 }
 
 void build(vector<vector<int> >& tree, vector<int>& data, int v, int tl, int tr) {
@@ -26,31 +35,40 @@ void build(vector<vector<int> >& tree, vector<int>& data, int v, int tl, int tr)
 	}
 }
 
-// int get_sum(vector<vector<int> >& tree, int v, int tl, int tr, int l, int r, bool is_odd) {
-// 	if (l > r)
-// 		return 0;
-// 	if (l == tl && r == tr)
-//         if (is_odd){
-//             return tree[v].first.first+ tree[v].second;
-//         }else{
-//             return tree[v].first.second+ tree[v].second;
-//         }
-// 	int tm = (tl + tr) / 2;
+ int get_sum(vector<vector<int> >& tree, int v, int tl, int tr, int l, int r, bool is_odd) {
+ 	if (l > r)
+ 		return 0;
+    increment_val(tree, v, tl,tr);
+ 	if (l == tl && r == tr)
+         if (is_odd){
+             return tree[v][0];
+         }else{
+             return tree[v][2];
+         }
+ 	int tm = (tl + tr) / 2;
 
-// 	return get_sum (tree, v*2, tl, tm, l, min(r,tm), is_odd)
-// 		+ get_sum (tree, v*2+1, tm+1, tr, max(l,tm+1), r, is_odd);
-// }
+ 	return get_sum (tree, v*2, tl, tm, l, min(r,tm), is_odd)
+ 		+ get_sum (tree, v*2+1, tm+1, tr, max(l,tm+1), r, is_odd);
+ }
 
 void update(vector<vector<int> >& tree, int v, int tl, int tr, int pos, int new_val) {
-	if (tl == tr)
-		tree[v] = get_pair_value(new_val);
-	else {
+    if (tl == tr) {
+        tree[v] = get_pair_value(new_val);
+    } else {
+        increment_val(tree, v,tl,tr);
 		int tm = (tl + tr) / 2;
-		if (pos <= tm)
-			update (tree,v*2, tl, tm, pos, new_val);
-		else
-			update (tree,v*2+1, tm+1, tr, pos, new_val);
-		tree[v] = {tree[v*2][0] + tree[v*2+1][0], tree[v*2][1] + tree[v*2+1][1],tree[v*2][2] + tree[v*2+1][2],tree[v*2][3] + tree[v*2+1][3],0};
+        if (pos <= tm) {
+            increment_val(tree, v * 2, tl, tm);
+            tree[v] = { tree[v][0] - tree[v * 2][0], tree[v][1] - tree[v * 2][1] ,tree[v][2] - tree[v * 2][2] ,tree[v][3] - tree[v * 2][3] ,0 };
+            update(tree, v * 2, tl, tm, pos, new_val);
+            tree[v] = {tree[v][0] + tree[v * 2][0], tree[v][1] + tree[v * 2][1] ,tree[v][2] + tree[v * 2][2] ,tree[v][3] + tree[v * 2][3] ,0 };
+        }
+        else {
+            increment_val(tree, v * 2+1, tm+1, tr);
+            tree[v] = { tree[v][0] - tree[v * 2 + 1][0], tree[v][1] - tree[v * 2 + 1][1] ,tree[v][2] - tree[v * 2 + 1][2] ,tree[v][3] - tree[v * 2 + 1][3] ,0 };
+            update(tree, v * 2 + 1, tm + 1, tr, pos, new_val);
+            tree[v] = { tree[v][0] + tree[v * 2 + 1][0], tree[v][1] + tree[v * 2 + 1][1] ,tree[v][2] + tree[v * 2 + 1][2] ,tree[v][3] + tree[v * 2 + 1][3] ,0 };
+        }
 	}
 }
 
@@ -58,31 +76,35 @@ void increment_area(vector<vector<int> >& tree, int v, int tl, int tr, int l, in
     if (l > r)
 		return;
 	if (l == tl && r == tr){
-        cout<<tl<<endl<<tr;
+        tree[v][4] += 1;
+        increment_val(tree, v, tl, tr);
         return;
     }
+    increment_val(tree, v,tl,tr);
 	int tm = (tl + tr) / 2;
 	increment_area(tree, v*2, tl, tm, l, min(r,tm));
     increment_area(tree, v*2+1, tm+1, tr, max(l,tm+1), r);
+    tree[v] = { tree[v * 2][0] + tree[v * 2 + 1][0], tree[v * 2][1] + tree[v * 2 + 1][1],tree[v * 2][2] + tree[v * 2 + 1][2],tree[v * 2][3] + tree[v * 2 + 1][3],0 };
 }
+
+
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    ifstream in("input.txt");
+    //ifstream in("C:/Users/Kizenkov_V/source/repos/BSU_FAMCS_MasterDegree/1 semester/SDS/3. sum in sequence/input.txt");
     string data;
     string delimiter = " ";
     int n,q;
     size_t pos;
-    getline(in,data);
+    getline(cin,data);
     pos = data.find(delimiter);
     n = stoi(data.substr(0, pos));
     data.erase(0, pos + delimiter.length());
     q = stoi(data);
-    getline(in,data);
+    getline(cin,data);
 
     vector<int> arr;
-    // vector<pair<int,int> > tree(4*n);
     vector<vector<int> > tree(4*n);
 
 
@@ -94,7 +116,7 @@ int main(){
     build(tree, arr, 1, 0, n-1);
 
     for (int i = 0; i< q; i++){
-        getline(in,data);
+        getline(cin,data);
         arr.clear();
         while ((pos = data.find(delimiter)) != string::npos) {
             arr.push_back(stoi(data.substr(0, pos)));
@@ -104,22 +126,19 @@ int main(){
         switch (arr[0])
         {
         case 1:
-            update(tree, 1,0,n-1, arr[1], arr[2]);
+            update(tree, 1,0,n-1, arr[1]-1, arr[2]);
             break;
         case 2:
-            increment_area(tree, int 1, 0, n-1, arr[1], arr[2])
-            /* code */
+            increment_area(tree, 1, 0, n - 1, arr[1]-1, arr[2]-1);
             break;
         case 3:
-            /* code */
+            cout << get_sum(tree, 1, 0, n - 1, arr[1]-1, arr[2]-1, true)<<"\n";
             break;
-        
         case 4:
-            /* code */
+            cout << get_sum(tree, 1, 0, n - 1, arr[1]-1, arr[2]-1, false)<<"\n";
             break;
         
         }
     }
-
     return 0;
 }
